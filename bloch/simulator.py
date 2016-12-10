dde8c347b1491b438a30f3bd7abcacd9d14b0a21
kingaza <kingaza@gmail.com>
2016-12-10 18:11:49 +0800

    new example of rf pulse demostration

---
+++ bloch/simulator.py
@@ -0,0 +1,61 @@
+import numpy as np
+import scipy as sp
+
+from bloch.bloch_simulator import bloch_c
+
+from bloch.bloch_processing import NUMBER
+from bloch.bloch_processing import process_gradient_argument, process_time_points, process_off_resonance_arguments
+from bloch.bloch_processing import process_positions, process_magnetization, reshape_matrices
+
+def sim(b1, gr, tp, t1, t2, df, dp, mode, mx=None, my=None, mz=None):
+    """
+    Bloch simulation of rotations due to B1, gradient and
+    off-resonance, including relaxation effects.  At each time
+    point, the rotation matrix and decay matrix are calculated.
+    Simulation can simulate the steady-state if the sequence
+    is applied repeatedly, or the magnetization starting at m0.
+
+    INPUT:
+            b1 = (1xM) RF pulse in G.  Can be complex.
+            gr = ((1,2,or 3)xM) 1,2 or 3-dimensional gradient in G/cm.
+            tp = (1xM) time duration of each b1 and gr point, in seconds,
+                            or 1x1 time step if constant for all points
+                            or monotonically INCREASING endtime of each
+                            interval..
+            t1 = T1 relaxation time in seconds.
+            t2 = T2 relaxation time in seconds.
+            df = (1xN) Array of off-resonance frequencies (Hz)
+            dp = ((1,2,or 3)xP) Array of spatial positions (cm).  
+                    Width should match width of gr.
+            mode= Bitmask mode:
+                    Bit 0:  0-Simulate from start or M0, 1-Steady State
+                    Bit 1:  1-Record m at time points.  0-just end time.
+
+    (optional)
+            mx,my,mz (NxP) arrays of starting magnetization, where N
+                    is the number of frequencies and P is the number
+                    of spatial positions.
+
+    OUTPUT:
+            mx,my,mz = NxP arrays of the resulting magnetization
+                            components at each position and frequency.
+    """
+    if isinstance(b1, NUMBER):
+        b1 = np.ones(1) * b1
+    ntime = b1.size
+
+    grx, gry, grz = process_gradient_argument(gr, ntime)
+    tp = process_time_points(tp, ntime)
+    df, nf = process_off_resonance_arguments(df)
+    dx, dy, dz, n_pos = process_positions(dp)
+    mx, my, mz = process_magnetization(mx, my, mz, ntime, nf*n_pos, mode)
+
+    if (2 & mode):
+        ntout = ntime
+    else:
+        ntout = 1
+
+    bloch_c(b1.real, b1.imag, grx, gry, grz, tp, ntime, t1, t2, df, nf, dx, dy, dz, n_pos, mode, mx, my, mz)
+
+    reshape_matrices(mx, my, mz, ntout, n_pos, nf)
+    return mx, my, mz
---
+++ test/RFSeqTests.py
@@ -0,0 +1,63 @@
+import unittest
+
+import numpy as np
+import scipy as sp
+
+from bloch.simulator import sim
+from bloch.rf_seq import hard_pulses, sinc_pulse
+
+from BlochTest import get_data_with_key
+
+TEST_DIR = "test_data"
+
+class RFSeqTests(unittest.TestCase):
+    """
+    Tests the rf sequences included with the module.
+    """
+
+    def test_hard_pulses(self):
+        """
+        Tests generation of a series of hard pulses.
+        """
+
+        gamma = 26752
+        dt = 4e-6
+        b1_max = .16
+        expected_pulses = np.zeros(int(100e-3 / dt))
+        pulse_time = (np.pi / 2) / (gamma * .16)  
+        pulse_length = int(pulse_time / dt)
+        expected_pulses[:pulse_length] = b1_max
+        second_pulse = int(20e-3/dt)
+        expected_pulses[second_pulse:second_pulse+pulse_length] = b1_max
+
+        b1 = .16
+        flip_angle = np.ones(2) * np.pi / 2
+        spin_echo_pulses = hard_pulses(b1, flip_angle, 20e-3, 100e-3, 2, 4e-6)
+        self.assertTrue(np.allclose(expected_pulses, spin_echo_pulses))
+
+    def test_sinc_pulse(self):
+        """
+        Tests generation of sinc pulse.
+        """
+        expected_mxy = get_data_with_key(TEST_DIR, "rf_test", "mxy")
+
+        duration = 3.2e-3
+        dt = duration / 256
+        flip = np.pi / 2
+        tbw = 8
+
+        rf = sinc_pulse(tbw, flip, duration, dt)
+        g = np.ones(rf.size) * .6
+        dp = np.linspace(-2, 2, 512)
+        mx0 = np.zeros(512)
+        my0 = np.zeros(512)
+        mz0 = np.ones(512)
+
+        mx, my, mz = sim(rf, g, dt, 100, 100, 0, dp, 0, mx0, my0, mz0)
+        mxy = mx + 1.0j*my
+        np.allclose(expected_mxy, mxy)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
